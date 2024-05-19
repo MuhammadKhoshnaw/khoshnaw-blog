0d57aa5da37f51fba5687ac6089713d7b9b5d667
MuhammadXoshnaw <muhammad.xoshnaw.eng@gmail.com>
2024-05-19 22:56:22 +0300

    wrapping up localization-in-android-apps.mdx

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -22,6 +22,28 @@ const projectsData: Project[] = [
         imgSrc: '/static/images/fib.png',
         href: 'https://fib.iq/en',
     },
+    {
+        title: 'Secure Voice',
+        description: `Secure Voice is a messaging and voice-over-IP service that uses SIP protocol. 
+        In this project, we used a third-party open-source library called Linphone to use SIP for messaging, 
+        voice, and video communication.`,
+        imgSrc: '/static/images/secure-voice.png',
+    },
+    {
+        title: 'Network KPI Library',
+        description: `In this project, we created a third-party Android library for a telecommunication 
+        company with over 3,000,000+ users. The library was collecting customers’ RF information to 
+        detect network issues.`,
+        imgSrc: '/static/images/kpi.webp',
+        // href: '',
+    },
+    {
+        title: 'MyTV+',
+        description: `An Android IP TV application has over 1,000,000+ users. This application streams local tv 
+        channels with movies and tv series. The application works on a customised Android TV STB device.`,
+        imgSrc: '/static/images/mytv.png',
+        // href: '',
+    },
 ]
 
 export default projectsData
