ed358c12f3befec3deb8ab213d63d6308408b811
MuhammadXoshnaw <muhammad.xoshnaw.eng@gmail.com>
2024-05-19 10:45:50 +0300

    Customizing default.mdx, page.tsx and projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,27 +1,27 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+    title: string,
+    description: string,
+    href?: string,
+    imgSrc?: string,
 }
 
 const projectsData: Project[] = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    {
+        title: 'Shufer',
+        description: `Meet Shufer, an app designed to help you manage vehicle violations and associated
+         fees with ease. With a simple and intuitive interface, you can easily track outstanding 
+         violations and fines, ensuring you stay on top of your obligations.`,
+        imgSrc: '/static/images/shufer.png',
+        href: 'https://play.google.com/store/apps/details?id=com.khoshnaw.shufer&hl=en_US',
+    },
+    {
+        title: 'First Iraqi Bank',
+        description: `FIB is the first mobile banking app in Iraq. Users can download the app and open a bank 
+        account in minutes. The project has three applications, personal, business and corporate. 
+        All share a single code base that follows clean architecture and is modulated by features.`,
+        imgSrc: '/static/images/fib.png',
+        href: 'https://fib.iq/en',
+    },
 ]
 
 export default projectsData
