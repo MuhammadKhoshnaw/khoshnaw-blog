0461dd8669e0bb9e9dfbc185d7017ac19a38752f
MuhammadXoshnaw <muhammad.xoshnaw.eng@gmail.com>
2024-05-20 00:51:22 +0300

    some adjustments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -42,12 +42,12 @@ const siteMetadata = {
         //   googleAnalyticsId: '', // e.g. G-XXXXXXX
         // },
     },
-/*    newsletter: {
+    newsletter: {
         // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
         // Please add your .env file and modify it according to your selection
         provider: 'buttondown',
-    },*/
-/*    comments: {
+    },
+    comments: {
         // If you want to use an analytics provider you have to add it to the
         // content security policy in the `next.config.js` file.
         // Select a provider and use the environment variables associated to it
@@ -76,7 +76,7 @@ const siteMetadata = {
             // This corresponds to the `data-lang="en"` in giscus's configurations
             lang: 'en',
         },
-    },*/
+    },
     search: {
         provider: 'kbar', // kbar or algolia
         kbarConfig: {
